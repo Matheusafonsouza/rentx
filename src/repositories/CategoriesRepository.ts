f43d33c43263e9daf26263bd37f507bdbd6d90ab
Matheus Afonso <matheusafonsouza@gmail.com>
2021-03-23 01:49:04 -0300

    feat: create categories repository

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,30 @@
+import { Category } from "../model/category";
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO): Category {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+
+    return category;
+  }
+}
+
+export { CategoriesRepository };
--- src/routes/categories/categories.routes.ts
+++ src/routes/categories/categories.routes.ts
@@ -1,23 +1,18 @@
 import { Router } from "express";
 
-import { Category } from "../../model/category";
+import { CategoriesRepository } from "../../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
 
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
-
-  Object.assign(category, {
-    name,
+  const category = categoriesRepository.create({
     description,
-    created_at: new Date(),
+    name,
   });
-
-  categories.push(category);
 
   return response.status(201).json(category);
 });
