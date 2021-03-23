5fef90c2bcf69c1eebd89c2888bb0e68dc624d16
Matheus Afonso <matheusafonsouza@gmail.com>
2021-03-23 01:56:13 -0300

    chore: already exists for category

--- src/routes/categories/categories.routes.ts
+++ src/routes/categories/categories.routes.ts
@@ -9,6 +9,12 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: 'Category already exists.' });
+  }
+
   const category = categoriesRepository.create({
     description,
     name,
