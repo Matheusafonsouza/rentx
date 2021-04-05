2493a6211cd53032c2aae7e40a1d98f6aec061c6
Matheus Afonso <matheusafonsouza@gmail.com>
2021-04-05 18:45:11 -0300

    refactor: using dependency injection at import category

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -1,14 +1,16 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
 
 import { ImportCategoryUseCase } from './ImportCategoryUseCase';
 
 class ImportCategoryController {
-  constructor(private importCategoryUseCase: ImportCategoryUseCase) {}
-
-  handle(request: Request, response: Response): Response {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const importCategoryUseCase = container.resolve(ImportCategoryUseCase);
     const { file } = request;
 
-    this.importCategoryUseCase.execute(file);
+    console.log(file);
+
+    await importCategoryUseCase.execute(file);
 
     return response.status(200).json(file);
   }
