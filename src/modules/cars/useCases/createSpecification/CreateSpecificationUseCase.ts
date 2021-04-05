e0986e9c668078eb8c03219b192ae43f9482b6f8
Matheus Afonso <matheusafonsouza@gmail.com>
2021-04-05 01:48:20 -0300

    refactor: changed model to entities

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,4 +1,4 @@
-import { Specification } from '../../model/Specification';
+import { Specification } from '../../entities/Specification';
 import { ISpecificationRepository } from '../../repositories/ISpecificationRepository';
 
 interface IRequest {
