51ad83de00020bed1de31e9412fa9786fb10a4be
Matheus Afonso <matheusafonsouza@gmail.com>
2021-04-08 00:57:41 -0300

    feat: error handling on server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import swaggerUI from 'swagger-ui-express';
 
+import { AppError } from './errors/AppError';
 import routes from './routes';
 import swaggerFile from './swagger.json';
 
@@ -14,5 +16,20 @@ app.use(express.json());
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
 
 app.listen(3333, () => console.log('Server is running on port 3333!'));
