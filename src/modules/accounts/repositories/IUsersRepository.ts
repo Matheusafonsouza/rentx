867274b12f62e536fbba3f2557e1a529189b1b75
Matheus Afonso <matheusafonsouza@gmail.com>
2021-04-08 00:48:55 -0300

    feat: findbyid method for users repository

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -5,6 +5,7 @@ interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<User>;
   list(): Promise<User[]>;
   findByEmail(email: string): Promise<User>;
+  findById(id: string): Promise<User>;
 }
 
 export { IUsersRepository };
