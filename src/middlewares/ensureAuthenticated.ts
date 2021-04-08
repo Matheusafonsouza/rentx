538dda133fb74065239587bec34fecb788057264
Matheus Afonso <matheusafonsouza@gmail.com>
2021-04-08 00:49:06 -0300

    feat: ensureAuthenticated middleware

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Response, Request } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import { UsersRepository } from '../modules/accounts/repositories/implementations/UsersRepository';
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const { authorization } = request.headers;
+
+  if (!authorization) {
+    throw new Error('Token missing');
+  }
+
+  const [, token] = authorization.split(' ');
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      '4a853e19f585d34e93b6f2a9ee257658'
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error('Use does not exists.');
+    }
+
+    next();
+  } catch {
+    throw new Error('Invalid token');
+  }
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { categoriesRoutes } from './categories/categories.routes';
 import { sessionRoutes } from './session/session.routes';
 import { specificationsRoutes } from './specifications/specifications.routes';
@@ -7,9 +8,12 @@ import { usersRoutes } from './users/users.routes';
 
 const routes = Router();
 
-routes.use('/categories', categoriesRoutes);
-routes.use('/specifications', specificationsRoutes);
 routes.use('/users', usersRoutes);
 routes.use(sessionRoutes);
+
+routes.use(ensureAuthenticated);
+
+routes.use('/categories', categoriesRoutes);
+routes.use('/specifications', specificationsRoutes);
 
 export default routes;
