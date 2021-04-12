48e53ec1a929f9ff59e13557864140a504455c74
Matheus Afonso <matheusafonsouza@gmail.com>
2021-04-12 11:54:22 -0300

    refactor: using typeorm on cars entity

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,24 +1,49 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 
+import { Category } from './Category';
+
+@Entity('cars')
 class Car {
+  @PrimaryColumn()
   id: string;
 
+  @Column()
   name: string;
 
+  @Column()
   description: string;
 
+  @Column()
   license_plate: string;
 
+  @Column()
   daily_rate: number;
 
+  @Column()
   fine_amount: number;
 
+  @Column()
   brand: string;
 
+  @Column()
   category_id: string;
 
-  available: true;
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
 
+  @Column()
+  available: boolean;
+
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {
