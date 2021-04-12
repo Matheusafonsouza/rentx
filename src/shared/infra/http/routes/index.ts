8fabe6b0e8ab704678463760a7ff8f6bb458d89d
Matheus Afonso <matheusafonsouza@gmail.com>
2021-04-12 11:55:20 -0300

    feat: create car route

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
 
+import { carsRoutes } from './cars/cars.routes';
 import { categoriesRoutes } from './categories/categories.routes';
 import { sessionRoutes } from './session/session.routes';
 import { specificationsRoutes } from './specifications/specifications.routes';
@@ -16,5 +17,6 @@ routes.use(ensureAuthenticated);
 
 routes.use('/categories', categoriesRoutes);
 routes.use('/specifications', specificationsRoutes);
+routes.use('/cars', carsRoutes);
 
 export default routes;
