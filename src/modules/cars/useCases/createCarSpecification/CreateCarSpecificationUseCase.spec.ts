c86de5ecc8001f7111e0ea360db8366223bc60e2
Matheus Afonso <matheusafonsouza@gmail.com>
2021-04-14 01:53:34 -0300

    feat: finished create car specification use case

--- src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
@@ -1,16 +1,20 @@
 import { CarsRepositoryMock } from '@modules/cars/repositories/mocks/CarsRepositoryMock';
+import { SpecificationRepositoryMock } from '@modules/cars/repositories/mocks/SpecificationRepositoryMock';
 import { AppError } from '@shared/errors/AppError';
 
 import { CreateCarSpecificationUseCase } from './CreateCarSpecificationUseCase';
 
 let createCarSpecificationUseCase: CreateCarSpecificationUseCase;
 let carsRepositoryMock: CarsRepositoryMock;
+let specificationRepositoryMock: SpecificationRepositoryMock;
 
 describe('Create Car Specification', () => {
   beforeEach(() => {
     carsRepositoryMock = new CarsRepositoryMock();
+    specificationRepositoryMock = new SpecificationRepositoryMock();
     createCarSpecificationUseCase = new CreateCarSpecificationUseCase(
-      carsRepositoryMock
+      carsRepositoryMock,
+      specificationRepositoryMock
     );
   });
 
@@ -36,11 +40,19 @@ describe('Create Car Specification', () => {
       license_plate: 'abc-1234',
     });
 
-    const specifications_id = ['12312'];
+    const specification = await specificationRepositoryMock.create({
+      description: 'test description',
+      name: 'test name',
+    });
 
-    await createCarSpecificationUseCase.execute({
+    const specifications_id = [specification.id];
+
+    const specificationsCars = await createCarSpecificationUseCase.execute({
       car_id: car.id,
       specifications_id,
     });
+
+    expect(specificationsCars).toHaveProperty('specifications');
+    expect(specificationsCars.specifications.length).toBe(1);
   });
 });
--- src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
@@ -1,4 +1,6 @@
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { ISpecificationRepository } from '@modules/cars/repositories/ISpecificationRepository';
 import { AppError } from '@shared/errors/AppError';
 
 interface IRequest {
@@ -7,14 +9,27 @@ interface IRequest {
 }
 
 class CreateCarSpecificationUseCase {
-  constructor(private carsRepository: ICarsRepository) {}
+  constructor(
+    private carsRepository: ICarsRepository,
+    private specificationRepository: ISpecificationRepository
+  ) {}
 
-  async execute({ car_id, specifications_id }: IRequest): Promise<void> {
+  async execute({ car_id, specifications_id }: IRequest): Promise<Car> {
     const carExists = await this.carsRepository.findById(car_id);
 
     if (!carExists) {
       throw new AppError('Car does not exists.');
     }
+
+    const specifications = await this.specificationRepository.findByIds(
+      specifications_id
+    );
+
+    carExists.specifications = specifications;
+
+    await this.carsRepository.create(carExists);
+
+    return carExists;
   }
 }
 
