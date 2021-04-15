9be5d809d160037a26c3ddf982381c64ed08e239
Matheus Afonso <matheusafonsouza@gmail.com>
2021-04-15 02:42:03 -0300

    feat: create rental use case and spec basic structure

---
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -0,0 +1,21 @@
+import { RentalsRepositoryMock } from '@modules/rentals/repositories/mocks/RentalsRepositoryMock';
+
+import { CreateRentalUseCase } from './CreateRentalUseCase';
+
+let createRentalUseCase: CreateRentalUseCase;
+let rentalsRepository: RentalsRepositoryMock;
+
+describe('Create Rental', () => {
+  beforeEach(() => {
+    rentalsRepository = new RentalsRepositoryMock();
+    createRentalUseCase = new CreateRentalUseCase(rentalsRepository);
+  });
+
+  it('Should be able to create a new rental', async () => {
+    await createRentalUseCase.execute({
+      car_id: 'car-id',
+      user_id: 'user-id',
+      expected_return_date: new Date(),
+    });
+  });
+});
