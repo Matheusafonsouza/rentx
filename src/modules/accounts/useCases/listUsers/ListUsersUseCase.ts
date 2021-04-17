22d5490c657fca2c4acf66191932bf016786f7a1
Matheus Afonso <matheusafonsouza@gmail.com>
2021-04-17 12:41:07 -0300

    fix: fix entities imports

--- src/modules/accounts/useCases/listUsers/ListUsersUseCase.ts
+++ src/modules/accounts/useCases/listUsers/ListUsersUseCase.ts
@@ -1,6 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 
-import { User } from '@modules/accounts/entities/User';
+import { User } from '@modules/accounts/infra/typeorm/entities/User';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
 
 @injectable()
--- src/modules/cars/useCases/listSpecifications/ListSpecificationsUseCase.ts
+++ src/modules/cars/useCases/listSpecifications/ListSpecificationsUseCase.ts
@@ -1,6 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 
-import { Specification } from '@modules/cars/entities/Specification';
+import { Specification } from '@modules/cars/infra/typeorm/entities/Specification';
 import { ISpecificationRepository } from '@modules/cars/repositories/ISpecificationRepository';
 
 @injectable()
