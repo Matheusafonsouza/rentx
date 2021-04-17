68889585e54631803ad2453bd5240d172d64708f
Matheus Afonso <matheusafonsouza@gmail.com>
2021-04-17 13:07:52 -0300

    feat: create category super test

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -1,13 +1,47 @@
+import { hash } from 'bcrypt';
 import request from 'supertest';
+import { Connection } from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
 
 import { app } from '@shared/infra/http/app';
+import createConnection from '@shared/infra/typeorm';
+
+let connection: Connection;
 
 describe('Create category controller', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = uuidV4();
+    const password = await hash('admin', 8);
+    await connection.query(
+      `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
+        values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'xxxxxx')
+      `
+    );
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
   it('Should be able to create a category', async () => {
-    const response = await request(app).get('/categories').send({
-      name: 'category test',
-      description: 'category test description',
+    const responseToken = await request(app).post('/sessions').send({
+      email: 'admin@rentx.com.br',
+      password: 'admin',
     });
+
+    const { token } = responseToken.body;
+
+    const response = await request(app)
+      .post('/categories')
+      .send({
+        name: 'category test',
+        description: 'category test description',
+      })
+      .set({ authorization: `Bearer ${token}` });
 
     expect(response.status).toBe(201);
   });
