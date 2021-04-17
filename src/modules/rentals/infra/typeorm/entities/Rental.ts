95f861580cfccea6fc3dea0278c4effd6b268431
Matheus Afonso <matheusafonsouza@gmail.com>
2021-04-16 22:56:09 -0300

    feat: using typeorm on rental entity

--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -1,22 +1,52 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 
+import { User } from '@modules/accounts/infra/typeorm/entities/User';
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
+
+@Entity('rentals')
 class Rental {
+  @PrimaryColumn()
   id: string;
 
+  @OneToOne(() => Car)
+  @JoinColumn({ name: 'car_id' })
+  car: Car;
+
+  @Column()
   car_id: string;
 
+  @OneToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @Column()
   user_id: string;
 
+  @Column()
   total: number;
 
+  @Column()
   start_date: Date;
 
+  @Column()
   end_date: Date;
 
+  @Column()
   expected_return_date: Date;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 
   constructor() {
