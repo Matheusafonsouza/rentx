aed09d41153fa8572e6afc563bb6eef685b2d51f
Matheus Afonso <matheusafonsouza@gmail.com>
2021-04-17 12:59:11 -0300

    refactor: refactor connection for test

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -3,7 +3,12 @@ import request from 'supertest';
 import { app } from '@shared/infra/http/app';
 
 describe('Create category controller', () => {
-  it('Should be ablle to create a category', async () => {
-    await request(app).get('/categories').expect(201);
+  it('Should be able to create a category', async () => {
+    const response = await request(app).get('/categories').send({
+      name: 'category test',
+      description: 'category test description',
+    });
+
+    expect(response.status).toBe(201);
   });
 });
