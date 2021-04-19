6cc347a5d2f66e541af7be6fcedb7374b9072885
Matheus Afonso <matheusafonsouza@gmail.com>
2021-04-19 00:56:16 -0300

    feat: list rentals by user controller and route

--- src/shared/infra/http/routes/rentals/index.ts
+++ src/shared/infra/http/routes/rentals/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { CreateRentalController } from '@modules/rentals/useCases/createRental/CreateRentalController';
 import { DevolutionRentalController } from '@modules/rentals/useCases/devolutionRental/DevolutionRentalController';
+import { ListRentalsByUserController } from '@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserUseController';
 
 import { ensureAuthenticated } from '../../middlewares/ensureAuthenticated';
 
@@ -9,6 +10,9 @@ const rentalsRouter = Router();
 
 const createRentalController = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
+
+rentalsRouter.get('/', ensureAuthenticated, listRentalsByUserController.handle);
 
 rentalsRouter.post('/', ensureAuthenticated, createRentalController.handle);
 rentalsRouter.post(
