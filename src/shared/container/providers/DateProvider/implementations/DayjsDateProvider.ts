1a02a1bba5c58231c228852781e7a5a057ab9d73
Matheus Afonso <matheusafonsouza@gmail.com>
2021-04-19 00:26:30 -0300

    feat: compare in days date provider

--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -12,6 +12,12 @@ class DayjsDateProvider implements IDateProvider {
     return dayjs(end_date_utc).diff(start_date_utc, 'hours');
   }
 
+  compareInDays(start_date: Date, end_date: Date): number {
+    const end_date_utc = this.convertToUtc(end_date);
+    const start_date_utc = this.convertToUtc(start_date);
+    return dayjs(end_date_utc).diff(start_date_utc, 'days');
+  }
+
   convertToUtc(date: Date): string {
     return dayjs(date).utc().local().format();
   }
