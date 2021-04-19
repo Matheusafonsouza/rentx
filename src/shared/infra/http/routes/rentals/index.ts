766f5c1e0b79b35ca9db3859dd22f3e8df964ae3
Matheus Afonso <matheusafonsouza@gmail.com>
2021-04-19 00:45:53 -0300

    feat: devolution rental controller and route

--- src/shared/infra/http/routes/rentals/index.ts
+++ src/shared/infra/http/routes/rentals/index.ts
@@ -1,13 +1,20 @@
 import { Router } from 'express';
 
 import { CreateRentalController } from '@modules/rentals/useCases/createRental/CreateRentalController';
+import { DevolutionRentalController } from '@modules/rentals/useCases/devolutionRental/DevolutionRentalController';
 
 import { ensureAuthenticated } from '../../middlewares/ensureAuthenticated';
 
 const rentalsRouter = Router();
 
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
 rentalsRouter.post('/', ensureAuthenticated, createRentalController.handle);
+rentalsRouter.post(
+  '/devolution/:id',
+  ensureAuthenticated,
+  devolutionRentalController.handle
+);
 
 export { rentalsRouter };
