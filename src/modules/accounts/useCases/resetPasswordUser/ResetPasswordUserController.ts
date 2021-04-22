b3a5dcc46912031d4454601f151ed74418195866
Matheus Afonso <matheusafonsouza@gmail.com>
2021-04-22 00:18:41 -0300

    feat: reset password

---
+++ src/modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController.ts
@@ -0,0 +1,21 @@
+import { Response, Request } from 'express';
+import { container } from 'tsyringe';
+
+import { ResetPasswordUserUseCase } from './ResetPasswordUserUseCase';
+
+class ResetPasswordUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { token } = request.query;
+    const { password } = request.body;
+
+    const resetPasswordUserUseCase = container.resolve(
+      ResetPasswordUserUseCase
+    );
+
+    await resetPasswordUserUseCase.execute({ password, token: String(token) });
+
+    return response.send();
+  }
+}
+
+export { ResetPasswordUserController };
--- src/shared/infra/http/routes/session/session.routes.ts
+++ src/shared/infra/http/routes/session/session.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { AuthenticateUserController } from '@modules/accounts/useCases/authenticateUser/AuthenticateUserController';
 import { RefreshTokenController } from '@modules/accounts/useCases/refreshToken/RefreshTokenController';
+import { ResetPasswordUserController } from '@modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController';
 import { SendForgotPasswordMailController } from '@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController';
 
 const sessionRoutes = Router();
@@ -9,9 +10,11 @@ const sessionRoutes = Router();
 const authenticateUserController = new AuthenticateUserController();
 const refreshTokenController = new RefreshTokenController();
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+const resetPasswordUserController = new ResetPasswordUserController();
 
 sessionRoutes.post('/sessions', authenticateUserController.handle);
 sessionRoutes.post('/refresh-token', refreshTokenController.handle);
 sessionRoutes.post('/forgot-password', sendForgotPasswordMailController.handle);
+sessionRoutes.post('/reset-password', resetPasswordUserController.handle);
 
 export { sessionRoutes };
