66bd69bf0de58d83d98a7a75b098468b8d202b3b
Matheus Afonso <matheusafonsouza@gmail.com>
2021-04-22 00:02:50 -0300

    fix: using handlebars template on forgot password

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import handlebars from 'handlebars';
 import nodemailer, { Transporter } from 'nodemailer';
 import { injectable } from 'tsyringe';
 
@@ -26,13 +28,22 @@ class EtherealMailProvider implements IMailProvider {
       .catch((err) => console.log(err));
   }
 
-  async sendMail(to: string, subject: string, body: string): Promise<void> {
+  async sendMail(
+    to: string,
+    subject: string,
+    variables: any,
+    path: string
+  ): Promise<void> {
+    const templateFileContent = fs.readFileSync(path).toString('utf-8');
+    const templateParse = handlebars.compile(templateFileContent);
+
+    const templateHTML = templateParse(variables);
+
     const message = await this.client.sendMail({
       to,
       from: 'Rentx <noreplay@rentx.com.br>',
       subject,
-      text: body,
-      html: body,
+      html: templateHTML,
     });
 
     console.log(`Message sent: ${message.messageId}`);
