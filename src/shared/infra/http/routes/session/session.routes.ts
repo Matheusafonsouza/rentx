9a6a705b24c0d366571d4ee77ec3856297ff1aa5
Matheus Afonso <matheusafonsouza@gmail.com>
2021-04-21 23:33:20 -0300

    feat: send forgot password mail

--- src/shared/infra/http/routes/session/session.routes.ts
+++ src/shared/infra/http/routes/session/session.routes.ts
@@ -2,13 +2,16 @@ import { Router } from 'express';
 
 import { AuthenticateUserController } from '@modules/accounts/useCases/authenticateUser/AuthenticateUserController';
 import { RefreshTokenController } from '@modules/accounts/useCases/refreshToken/RefreshTokenController';
+import { SendForgotPasswordMailController } from '@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController';
 
 const sessionRoutes = Router();
 
 const authenticateUserController = new AuthenticateUserController();
 const refreshTokenController = new RefreshTokenController();
+const sendForgotPasswordMailController = new SendForgotPasswordMailController();
 
 sessionRoutes.post('/sessions', authenticateUserController.handle);
 sessionRoutes.post('/refresh-token', refreshTokenController.handle);
+sessionRoutes.post('/forgot-password', sendForgotPasswordMailController.handle);
 
 export { sessionRoutes };
